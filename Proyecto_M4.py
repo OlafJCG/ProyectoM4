ee0a7fd64887f2aa529f2a94396dea1fb2fa0b24
OLAF CRUZ <chucholaf@hotmail.com>
2022-11-20 17:15:18 -0600

    Código comentado.

--- Proyecto_M4.py
+++ Proyecto_M4.py
@@ -25,17 +25,23 @@ def impresion_listas (lista):
 
 def llama_api ():
     """
-    Función que hará una llamada a la API.
+    Función que hará una llamada a la API y de aquí se desprenderá el resto del programa.
+    Se repetirá la pregunta del pokémon si no se encuentra (estatus 404) y se cerrará el programa si nos arroja una respuesta diferente.
     """
+    # CICLO PARA SEGUIR PREGUNTANDO EL NOMBRE DE UN POKÉMON EN CASO DE QUE OBTENGAMOS UNA RESPUESTA DIFERENTE AL ESTATUS 200.
     while True:
         pokemon = input("Escribe el nombre de un Pokémon: ").lower()
         url = "https://pokeapi.co/api/v2/pokemon/" + pokemon
         respuesta = requests.get(url)
+        # VALIDAR SI SE ENCONTRÓ EL POKÉMON
         if respuesta.status_code == 200:
             print("¡GENIAL!. Pokémon encontrado.")
+            # GUARDAMOS LOS DATOS DEL POKÉMON EN LA VARIABLE "datos"
             datos = respuesta.json()
+            # LLAMADA DE LA FUNCIÓN "muestra_poke()" ENVIAMOS DE PARAMETROS LOS DATOS DEL POKÉMON
             muestra_poke(datos)
             break
+        # ELIF PARA QUE SI NO ENCONTRÓ EL POKÉMON, EL USUARIO VUELVA A INTRODUCIR UN NOMBRE DE OTRO POKÉMON.
         elif respuesta.status_code == 404:
             print("Pokémon no encontrado, lo siento. \nPero puedes emprender una busqueda y nombrar así a un pokémon nunca antes visto. \nVuelve a intentar con otro Pokémon.")
         else:
@@ -45,19 +51,25 @@ def llama_api ():
 def muestra_poke(datos):
     """
     Función que nos mostrará la imagen del Pokémon y algunas estadísticas (peso, tamaño, movimientos, habilidades y tipos.)
+    También hara la llamada de una función para guardar la información del pokémon.
     """
+    # MANEJO DE EXCEPCIONES POR SI NO HAY UNA IMAGEN DEL POKÉMON.
     try:
         url_imagen = datos["sprites"]["front_default"]
+        # GUARDAMOS LA IMAGEN EN LA VARIABLE "imagen" utilizando la librería "PIL" Y EL MÓDULO "Image" y lad funciones "open" y "urlopen"
         imagen = Image.open(urlopen(url_imagen))
     except:
         print("El Pokémon no tiene imagen.")
+    # GUARDAMOS LOS DATOS QUE NOS INTERESAN EN VARIABLES.
     nombre = datos["name"]
     peso = datos["weight"]
     tamano = datos["height"]
+    #LLAMAMOS A UNA FUNCIÓN PARA NO REPETIR EN CÓDIGO LA CREACIÓN DE LAS LISTAS CON LOS MOVIMIENTOS, HABILIDADES Y SU TIPO O TIPOS DEL POKÉMON.
     movimientos = creaLista(datos["moves"], "move", "name")
     habilidades = creaLista(datos["abilities"], "ability", "name")
     tipos = creaLista(datos["types"], "type", "name")
     
+    #CREACIÓN DE NUESTRO DICCIONARIO.
     datosPoke = {
         "Nombre":nombre, 
         "url_imagen" : url_imagen, 
@@ -67,7 +79,6 @@ def muestra_poke(datos):
         "Habilidades" : habilidades, 
         "Tipos" : tipos
         }
-    guarda_poke (datosPoke)
 
     # IMPRESIÓN DE LA INFORMACIÓN DEL POKÉMON
     print("Movimientos: ")
@@ -87,12 +98,15 @@ def muestra_poke(datos):
     # ESTAS LÍNEAS NOS MUESTRAN LA IMAGEN DEL POKÉMON
     imgplot = plt.imshow(imagen)
     plt.show()
+    # LLAMAMOS A LA FUNCIÓN "guarda_poke()" con el diccionario de los datos del pokémon como parámetro.
+    guarda_poke (datosPoke)
     
 def guarda_poke(datos):
     """
-    Función que nos guargará el Pokémon en un archivo .JSON
+    Función que nos guargará el Pokémon en un archivo .JSON.
+    En caso de que el pokémon ya se encuentre en nuestro "pokédex" ya no lo guardaremos.
     """
-    #Manejo de excepciones por si no tenemos creado el archivo "pokemones.json
+    #Manejo de errores por si no tenemos creado el archivo "pokemones.json
     try:
         with open ("ProyectoM4/pokédex/pokemones.json", "r") as f_pokemones:
             datosPokemones = json.load(f_pokemones)
@@ -112,7 +126,9 @@ def guarda_poke(datos):
     with open ("ProyectoM4/pokédex/pokemones.json", "w") as f_pokemones:
         json.dump(datosPokemones, f_pokemones)
 
+# IMPRESIÓN DE BIENVENIDA AL PROGRAMA
 print ("""Bienvenido a tu Pokédex by Olaf.
 Disfruta de tu búsqueda.\n""")
 
+# LLAMADA DE LA FUNCIÓN "llama_api()" PARA COMENZAR EL PROGRAMA.
 llama_api()
